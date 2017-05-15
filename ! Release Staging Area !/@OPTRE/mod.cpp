80a395f608a038b2750012eb55ae53dca82e43b2
Scorch052 <misterhipster052@gmail.com>
2017-05-15 16:37:35 -0500

    Cloning Chunk 1

---
+++ ! Release Staging Area !/@OPTRE/mod.cpp
@@ -0,0 +1,7 @@
+name = "Operation: TREBUCHET";
+picture = "\OPTRE_Core\Data\logo_large_ca.paa";
+hidePicture = "false";
+hideName = "false";
+actionName = "Reddit";
+action = "https://www.reddit.com/r/OperationTrebuchet/";
+description = "Halo Total Modification for ArmA 3";
---
+++ ! Release Staging Area !/@OPTRE_FC/mod.cpp
@@ -0,0 +1,7 @@
+name = "Operation: TREBUCHET First Contact";
+picture = "\OPTRE_FC_Core\Data\logo_large_ca.paa";
+hidePicture = "false";
+hideName = "false";
+actionName = "Reddit";
+action = "https://www.reddit.com/r/OperationTrebuchet/";
+description = "Post-Insurrection expansion for Operation: TREBUCHET";
